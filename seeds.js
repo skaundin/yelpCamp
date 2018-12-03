38b9f72af3e1a2788feedbdce93f4366bf18417d
Suchita <suchita.kaundin@gmail.com>
2018-12-03 19:11:42 +0000

    initial commit

---
+++ seeds.js
@@ -0,0 +1,52 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    
+    { name: "Cloud Nest",
+      image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    { name: "Desert Mesa",
+      image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+     { name: "Canyon Floor",
+      image: "https://images.pexels.com/photos/176381/pexels-photo-176381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+     ]
+     
+function seedDB(){
+
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Removed Campgrounds");
+            data.forEach(function(seed){
+            Campground.create(seed, function(err,campground){
+            if(err){
+                console.log(err);
+            }else {
+                console.log("Added a campground");
+                Comment.create({
+                    text: "This is a great campground but no internet",
+                    author: "Homer"
+                },function(err,comment){
+                    if(err){
+                        console.log(err);
+                        }else{
+                            campground.comments.push(comment);
+                            campground.save();
+                        }
+                    });
+                }
+                
+                });
+            
+                });
+            }
+            });
+}
+
+
+module.exports = seedDB;

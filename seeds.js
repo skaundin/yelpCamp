b755f0479152303a7ddca26a8de785875d659cf1
Suchita <suchita.kaundin@gmail.com>
2019-04-27 17:06:40 +0000

    Added Background slider

--- seeds.js
+++ seeds.js
@@ -5,7 +5,7 @@ var Comment = require("./models/comment");
 var data = [
     
     { name: "Cloud Nest",
-      image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      image: "http://i.imgur.com/K3mPv14.jpg",
       description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
     { name: "Desert Mesa",
       image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
